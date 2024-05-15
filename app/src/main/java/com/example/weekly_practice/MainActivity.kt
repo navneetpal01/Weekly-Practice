afba06e2c9b9d45168732cc7638d12697ad77e8c
navneet <navneetpalgithub@gmail.com>
2024-05-15 10:24:09 +0530

    15

---
+++ app/src/main/java/com/example/weekly_practice/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.weekly_practice
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
