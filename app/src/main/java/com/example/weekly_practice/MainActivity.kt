0020934d1110b6e1110969c92c489e4e31f09c81
navneet <navneetpalgithub@gmail.com>
2024-05-15 10:26:18 +0530

    15

--- app/src/main/java/com/example/weekly_practice/MainActivity.kt
+++ app/src/main/java/com/example/weekly_practice/MainActivity.kt
@@ -6,8 +6,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.HiltAndroidApp
 
-
+@HiltAndroidApp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge(
