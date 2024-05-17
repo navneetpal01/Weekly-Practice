2064bb937d10c0bf68d78ae7a169d206d91cb83e
navneet <navneetpalgithub@gmail.com>
2024-05-17 11:44:27 +0530

    15

--- app/src/main/java/com/example/weekly_practice/MainActivity.kt
+++ app/src/main/java/com/example/weekly_practice/MainActivity.kt
@@ -7,8 +7,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/weekly_practice/NotesApplication.kt
@@ -0,0 +1,10 @@
+package com.example.weekly_practice
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NotesApplication : Application(){
+
+
+}
