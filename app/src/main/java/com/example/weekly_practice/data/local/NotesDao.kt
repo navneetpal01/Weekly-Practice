c6c5b82b26b4d0653b3f4f5638af6663698e7f7d
navneet <navneetpalgithub@gmail.com>
2024-05-17 12:12:38 +0530

    15

---
+++ app/src/main/java/com/example/weekly_practice/data/local/NotesDao.kt
@@ -0,0 +1,35 @@
+package com.example.weekly_practice.data.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface NotesDao{
+
+
+    @Upsert
+    fun upsertNote(note : Note)
+
+
+    @Delete
+    fun deleteNote(note : Note)
+
+
+    @Query("SELECT * FROM NOTE ORDER BY dateAdded")
+    fun getNotes() : Flow<List<Note>>
+
+
+    @Query("SELECT * FROM NOTE ORDER BY description ASC")
+    fun getNotesOrderByTittle() : Flow<List<Note>>
+
+
+
+
+
+
+
+}
