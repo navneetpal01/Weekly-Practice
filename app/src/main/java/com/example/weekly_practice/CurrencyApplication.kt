fcefa1b6179a06197cd929e3d300a57f20f5fff1
navneet <navneetpalgithub@gmail.com>
2024-05-18 10:46:52 +0530

    15

---
+++ app/src/main/java/com/example/weekly_practice/CurrencyApplication.kt
@@ -0,0 +1,9 @@
+package com.example.weekly_practice
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CurrencyApplication : Application(){
+
+}
---
+++ app/src/main/java/com/example/weekly_practice/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.weekly_practice
+
+import android.graphics.Color
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.weekly_practice.ui.theme.WeeklyPracticeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
+        super.onCreate(savedInstanceState, persistentState)
+        setContent {
+            WeeklyPracticeTheme {
+
+            }
+        }
+    }
+}
+
