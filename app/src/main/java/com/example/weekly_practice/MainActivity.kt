688a69464615c8583c93676301da0b83c40754e6
navneet <navneetpalgithub@gmail.com>
2024-05-26 11:48:25 +0530

    15

--- app/src/main/java/com/example/weekly_practice/MainActivity.kt
+++ app/src/main/java/com/example/weekly_practice/MainActivity.kt
@@ -7,11 +7,19 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.collectAsState
-import com.example.weekly_practice.model.Bpi
-import com.example.weekly_practice.model.Currency
-import com.example.weekly_practice.model.CurrencyDetails
-import com.example.weekly_practice.presentation.CurrencyScreen
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weekly_practice.presentation.CurrencyData
 import com.example.weekly_practice.ui.theme.WeeklyPracticeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,6 +29,7 @@ class MainActivity : ComponentActivity() {
 
     private val viewModel by viewModels<MainViewModel>()
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.light(
@@ -32,8 +41,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             val currency = viewModel.currencyData.collectAsState().value
             WeeklyPracticeTheme {
-                currency?.let {
-                    CurrencyScreen(bpi = it.bpi)
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = "Currency App",
+                                    fontSize = 30.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    style = TextStyle(
+                                        platformStyle = PlatformTextStyle(
+                                            includeFontPadding = false
+                                        )
+                                    )
+                                )
+                            }
+                        )
+                    }
+                ) { paddingValues ->
+                    currency?.let { currency ->
+
+                        CurrencyData(
+                            modifier = Modifier
+                                .padding(paddingValues),
+                            bpi = currency.bpi
+                        )
+
+                    }
                 }
             }
         }
